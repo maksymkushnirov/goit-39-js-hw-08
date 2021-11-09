1425f7ed66c9c54f58c58aac60cd51cfebed90d9
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-09 12:53:23 +0200

    minor improvements

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,9 @@
 import SimpleLightbox from 'simplelightbox';
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+// Add imports above this line
+
+// Change code below this line
 
 console.log(galleryItems);
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
