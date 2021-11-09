0ffcf317fec04b5958ada57b44cec17e56481f4b
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-09 04:18:59 +0200

    solve task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const arrayEl = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`,
+  '',
+);
+galleryEl.insertAdjacentHTML('afterbegin', arrayEl);
+
+const optionsForModal = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const modalFromLib = new SimpleLightbox('.gallery__link', optionsForModal);
