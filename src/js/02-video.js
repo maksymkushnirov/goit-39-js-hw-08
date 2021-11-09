f247bdfe2aca10b1fc19fdaaa80d04966e3f1e64
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-09 07:32:33 +0200

    solve task-02
    Vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
+player.on('play', function () {
+  console.log('played the video!');
+  player.on('timeupdate', throttle(recordingPlayingTime, 1000));
+});
+
+function recordingPlayingTime(event) {
+  console.log(event);
+  localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+getValueFromlocalStorage();
+
+function getValueFromlocalStorage() {
+  const valueLocalStorage = localStorage.getItem(STORAGE_KEY);
+  if (valueLocalStorage) {
+    player.setCurrentTime(valueLocalStorage);
+  }
+}
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
